24dc3c3f1fd10235aca4bc0cc382a9228daffbb6
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2021-12-07 21:52:04 +0200

    Script for  burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+  });
 
-})
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+});
