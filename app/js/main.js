b66bc3a05d96014a8380fe293fc74dc7328185f7
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2021-12-11 23:56:13 +0200

    Master feat (add script for sliders)

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 $(function () {
-  $(".header__btn").on("click", function () {
-    $(".rightside-menu").removeClass("rightside-menu--close");
-  });
 
   $(".rightside-menu__close").on("click", function () {
     $(".rightside-menu").addClass("rightside-menu--close");
+  });
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
   });
   $(".top__slider").slick({
     dots: true,
@@ -12,6 +12,20 @@ $(function () {
     fade: true,
     autoplay: true,
   });
+
+  $(".contact-slider").slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    dots: true,
+    arrows: false,
+  });
+
+  $(".article-slider__box").slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="./images/arrow-slide-left.svg" alt="arrow left"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="./images/arrow-slide-right.svg" alt="arrow right"></button>',
+  });
+
+
   let mixer = mixitup(".gallery__inner", {
     load: {
       filter: ".living",
