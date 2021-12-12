5ce136f61757e54478e22decc2a4d41086fbaafa
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2021-12-12 22:43:34 +0200

    Master feat (Add script for burger menu and adaptive for slider)

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,17 @@ $(function () {
   $(".header__btn").on("click", function () {
     $(".rightside-menu").removeClass("rightside-menu--close");
   });
+
+ 
+  $(".header__btn-menu").on("click", function () {
+    $(".menu").toggleClass("menu--open");
+  });
+
+
+  if($(window).width() < 651){
+    $('.works-path__item--measuring').appendTo($('.works-path__items-box'));
+  }
+   
   $(".top__slider").slick({
     dots: true,
     arrows: false,
@@ -18,6 +29,52 @@ $(function () {
     slidesToScroll: 10,
     dots: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 8,
+        }
+      },
+      {
+        breakpoint: 1450,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+        }
+      },
+      {
+        breakpoint: 1180,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+        }
+      }
+
+    ]
   });
 
   $(".article-slider__box").slick({
